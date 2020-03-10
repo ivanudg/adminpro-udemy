9e83937bee6f9b6669429446811bc3dd86cba87f
Iván Uribe <ivanudg@hotmail.com>
2020-03-10 12:10:45 -0600

    Input, Output y viewChild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms"
 
 // Routes
 import { PAGES_ROUTES } from './pages.routes';
@@ -6,10 +7,17 @@ import { PAGES_ROUTES } from './pages.routes';
 // Modules
 import { SharedModule } from '../shared/shared.module';
 
+// ng2-Charts
+import { ChartsModule } from 'ng2-charts';
+
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+
+// Temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 
@@ -18,17 +26,21 @@ import { Graficas1Component } from './graficas1/graficas1.component';
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     // Todas las que estén dentro de Export podrán ser accesadas desde fuera de este módulo
     exports: [
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 
